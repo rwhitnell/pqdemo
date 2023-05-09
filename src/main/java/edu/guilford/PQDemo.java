7be11ff3bfdb92498e73b168d3538cb1d5d77ced
Rob Whitnell <rwhitnel@guilford.edu>
2023-05-09 08:46:21 -0400

    Include material from 5-5 class on Set

--- src/main/java/edu/guilford/PQDemo.java
+++ src/main/java/edu/guilford/PQDemo.java
@@ -2,6 +2,7 @@ package edu.guilford;
 
 import java.util.PriorityQueue;
 import java.util.Stack;
+import java.util.TreeSet;
 
 public class PQDemo {
     public static void main(String[] args) {
@@ -41,6 +42,39 @@ public class PQDemo {
         System.out.println("Is the stack empty? " + s.isEmpty());
         System.out.println("Is the priority queue empty? " + pq.isEmpty());
 
+                // Instantiate a TreeSet of FullName objects
+        TreeSet<FullName> nameSet = new TreeSet<FullName>();
+
+        // Add 20 FullName objects to the stack
+        for (int i = 0; i < 20; i++) {
+            s.push(new FullName());
+        }
+        System.out.println("Here's the new stack of FullName objects: ");
+        System.out.println(s);
+
+        // Add the elements of the stack to the TreeSet
+        while (!s.isEmpty()) {
+            // popping an element off the stack returns that element and removes it from the stack
+            // then we add it to the set
+            nameSet.add(s.pop());
+        }
+
+        // Print the TreeSet
+        System.out.println("Here's the TreeSet of FullName objects: ");
+        System.out.println(nameSet);
+        System.out.println("The size of the set is " + nameSet.size());
+
+        // Show that the Set is iterable
+        System.out.println("Here's the TreeSet of FullName objects using for-each: ");
+        for (FullName name : nameSet) {
+            System.out.println(name);
+        }
+        // Is the set empty after iterating over it?
+        System.out.println("Is the set empty? " + nameSet.isEmpty());
+        // Sets and Lists have non-destructive iteration; Stacks and Queues have destructive iteration
+        // Difference between destructive and non-destructive iteration is whether accessing the element
+        // removes it from the data structure
+ 
 
     }
 }
